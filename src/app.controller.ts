118da4c6effce221bce0a8502e25b96605278d1d
alexpro99 <acuevasferras@gmail.com>
2025-06-20 20:56:42 -0400

    fix: remove dead code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,31 +7,5 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly llmService: LlmService,
-  ) {}
-
-  @Get()
-  getHello(): any {
-    return this.appService.getHello();
-  }
-
-  @Post('llm')
-  async getLlmResponse(
-    @Body('userPrompt') userPrompt: string,
-    @Body('systemPrompt') systemPrompt: string,
-    @Body('variables') variables: Record<string, any>,
-  ): Promise<any> {
-    console.log({
-      userPrompt,
-      systemPrompt,
-      variables,
-    });
-
-    // const response = await this.llmService.getResponseFromOpenAI(
-    //   userPrompt,
-    //   systemPrompt,
-    //   variables,
-    // );
-
-    return 'en desarrollo'
-  }
+  ) { }
 }
